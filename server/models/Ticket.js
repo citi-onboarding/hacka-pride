090a63855b04d1f0ee19fb83910c4cb9b2b09988
josebasilioo <josebasiliosilvaneto@gmail.com>
2020-07-04 23:08:09 -0300

    feature(apiTicket): ticket url database

---
+++ server/models/Ticket.js
@@ -0,0 +1,11 @@
+const keystone = require('keystone');
+
+const { Types } = keystone.Field;
+
+const Ticket = new keystone.List('Link Ingresso')
+
+Ticket.add({
+    IngressoURL: { type: Types.Text, required: true, initial: true, index: true },
+});
+
+Ticket.register();
