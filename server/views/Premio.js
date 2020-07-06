3ca9fe21b8068c9d25faa728761802bb74288475
Ramomjcs <ramom1999@live.com>
2020-07-06 18:18:54 -0300

    hotfix(apiCollaborators): change informations about Company

--- server/views/Premio.js
+++ server/views/Premio.js
@@ -3,13 +3,22 @@ const Premio = keystone.list('Prêmios').model;
 const handlers = {
   getPremio: function (req, res) {
 
+    const objeto = {
+      dataCiti: {},
+      // dataParceiros: {},
+    }
+
     Premio.find().exec(function (err, data) {
       if (err) {
         console.log(err);
         res.status(500).send('DB Error');
       }
-      res.status(200).send(data);
+
+      objeto.dataCiti = data;
+
+      res.status(200).send(objeto.dataCiti);
     });
+
   }
 }
 module.exports = handlers; 
