32d4e32fefdf4f39a89b7d217e739a4010f14a33
Ramomjcs <ramom1999@live.com>
2020-07-06 12:20:43 -0300

    bugfix(modSocial): Alterações

--- server/models/HackaPride.js
+++ server/models/HackaPride.js
@@ -2,14 +2,9 @@ const keystone = require('keystone');
 
 const { Types } = keystone.Field;
 
-const HackaPride = new keystone.List('HackaPride', {
-    map: { name: 'key' },
-    nocreate: true,
-    nodelete: true
-  });
+const HackaPride = new keystone.List('HackaPride')
 
 HackaPride.add({
-    key: { type: Types.Text, value: 'Social', noedit: true },
     nome: { type: Types.Text, initial: true, required: true, index: true, noedit: true, },
     instagram: { type: Types.Text, initial: true, required: true, index: true, },
     facebook: { type: Types.Text, initial: true, required: true, index: true, },
