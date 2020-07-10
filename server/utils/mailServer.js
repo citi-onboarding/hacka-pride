ba95482fb59cf42e79644d7e8977052a8561fde1
josebasilioo <josebasiliosilvaneto@gmail.com>
2020-07-10 14:38:21 -0300

    bugfix(apiContato): fixing nodemailer errors

--- server/utils/mailServer.js
+++ server/utils/mailServer.js
@@ -11,8 +11,13 @@ module.exports = async ({
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
+            type: 'OAuth2',
             user: process.env.EMAIL,
             pass: process.env.PASSWORD,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN,
+            accessToken: process.env.ACCESS_TOKEN
         },
     });
 
