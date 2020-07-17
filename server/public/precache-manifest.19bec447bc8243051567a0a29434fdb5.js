afe6a694f470955d288b2aa5c4298a81e46b01eb
josebasilioo <josebasiliosilvaneto@gmail.com>
2020-07-17 16:38:08 -0300

    bugfix(toastContact): adding public folder

---
+++ server/public/precache-manifest.19bec447bc8243051567a0a29434fdb5.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ef7d4a51f74bbda80dc43a6ae45eaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbb415212a31b29f6903",
+    "url": "/static/css/2.7ea1f6dd.chunk.css"
+  },
+  {
+    "revision": "49d89ae448bb8a930020",
+    "url": "/static/css/main.fa23ed3d.chunk.css"
+  },
+  {
+    "revision": "fbb415212a31b29f6903",
+    "url": "/static/js/2.274a137e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.274a137e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d89ae448bb8a930020",
+    "url": "/static/js/main.92d088cd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "15c6548d6411dc503e96773ef4011cad",
+    "url": "/static/media/Divisória-mobile.15c6548d.svg"
+  },
+  {
+    "revision": "e512c7462c1d662ca873c0bc8115084b",
+    "url": "/static/media/Divisória.e512c746.svg"
+  },
+  {
+    "revision": "c524b059793b97cfcb5fbba61e1ee240",
+    "url": "/static/media/Facebook-mobile.c524b059.svg"
+  },
+  {
+    "revision": "3a0b85df61573fd0f7d95a4c85d333f2",
+    "url": "/static/media/Facebook.3a0b85df.svg"
+  },
+  {
+    "revision": "4571ec39ddc3ba3cf1b4af437d9633d3",
+    "url": "/static/media/Facebook.4571ec39.svg"
+  },
+  {
+    "revision": "bd0c212c6a2a4a63b8ff99760dcce81f",
+    "url": "/static/media/Facebook.bd0c212c.svg"
+  },
+  {
+    "revision": "ea1529dc85ddb69181078ac38e3a5462",
+    "url": "/static/media/Ilustração - Convidados.ea1529dc.svg"
+  },
+  {
+    "revision": "609a9cdfe3e0a680dead4e106e92b0f8",
+    "url": "/static/media/Ilustração - Navbar.609a9cdf.svg"
+  },
+  {
+    "revision": "58594f1415a050ec6ebc4fc55bb1efb2",
+    "url": "/static/media/Ilustração - Parceiros.58594f14.svg"
+  },
+  {
+    "revision": "bd255ab17bbbc8fbc633b73208e36c5e",
+    "url": "/static/media/Ilustração - mobile.bd255ab1.svg"
+  },
+  {
+    "revision": "0a00274313a5f74da284dcc1a1807dbb",
+    "url": "/static/media/Instagram-mobile.0a002743.svg"
+  },
+  {
+    "revision": "1ae6d50ba2ac2adbea079edcd8e82e19",
+    "url": "/static/media/Instagram.1ae6d50b.svg"
+  },
+  {
+    "revision": "240e5e9dc5758cd131cb69f8edd02f5d",
+    "url": "/static/media/Instagram.240e5e9d.svg"
+  },
+  {
+    "revision": "de84866ac897b03e202260f70e98979e",
+    "url": "/static/media/Instagram.de84866a.svg"
+  },
+  {
+    "revision": "392930bdbe085b662b54ea1bed986552",
+    "url": "/static/media/LinkedIn-mobile.392930bd.svg"
+  },
+  {
+    "revision": "4b3a804b41068c896958954edbf33c5f",
+    "url": "/static/media/LinkedIn.4b3a804b.svg"
+  },
+  {
+    "revision": "3f04a5f077f26950ece2d7ff691c6799",
+    "url": "/static/media/Logo.3f04a5f0.svg"
+  },
+  {
+    "revision": "bdc2921cf65442690bc4365975e6668e",
+    "url": "/static/media/LogoNav.bdc2921c.svg"
+  },
+  {
+    "revision": "d9393b8d54a662c68b61b345eb76e671",
+    "url": "/static/media/Mobile.d9393b8d.svg"
+  },
+  {
+    "revision": "53e06d19aeb71faad74ee245d693b091",
+    "url": "/static/media/globe.53e06d19.svg"
+  },
+  {
+    "revision": "b13fed6d867ed82e174c177ee351fc90",
+    "url": "/static/media/heart.b13fed6d.svg"
+  },
+  {
+    "revision": "3bf46535fb7fd7fc06d2a12599730dd0",
+    "url": "/static/media/next.3bf46535.svg"
+  },
+  {
+    "revision": "4a58318d0bcb9c55f5118f2484b41e5b",
+    "url": "/static/media/prev.4a58318d.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
