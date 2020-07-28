07aae57eab6dcfe1fe69ddc27e09d40ba2b584dc
josebasilioo <josebasiliosilvaneto@gmail.com>
2020-07-27 22:57:49 -0300

    feature(information): added css styles to information section

--- client/src/components/Information/Information.jsx
+++ client/src/components/Information/Information.jsx
@@ -8,12 +8,35 @@ function Information() {
     return (
         <div className="all-information-content">
             <h2>Informações sobre o evento</h2>
-            <div className="trophy-svg"></div>
+            <div className="trophy-svg" alt="ilustração de arco-íris e um casal segurando troféu"></div>
             <section className="data-content">
                 <div className="description-www">
-                    <p><div className="calendar-svg"></div>&nbsp;16, 17 e 18 de Outubro</p>
-                    <p><div className="local-svg"></div>Local a ser definido</p>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.</p>
+                    <div className="text-local-data font-styles">
+                        <div className="calendar-svg" alt="ilustração de calendário roxo"></div>
+                        <p className="alinhar-data">16, 17 e 18 de outubro</p>
+                    </div>
+                    <div className="text-local-data adjust-this font-styles">
+                        <div className="local-svg" alt="ilustração de calendário roxo"></div>
+                        <p className="alinhar-local">Local a ser definido</p>
+                    </div>
+                    <p className="text-description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.</p>
+                </div>
+                <div className="prizing">
+                    <h3>Premiações</h3>
+                    <div className="each-prize">
+                        <div className="second" alt="pódio segundo lugar">
+                            <h1 className="each-position podium-sec">2</h1>
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div className="first-prize" alt="pódio primeiro lugar">
+                            <h1 className="each-position podium-first">1</h1>
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div className="third" alt="pódio terceiro lugar">
+                            <h1 className="each-position podium-third">3</h1>
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
