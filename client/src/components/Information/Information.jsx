d95f825fa6ed08827182b8148ee01109231b7f0a
josebasilioo <josebasiliosilvaneto@gmail.com>
2020-07-28 19:51:19 -0300

    feature(information): finished css style and responsivity, fixed keystone errors

--- client/src/components/Information/Information.jsx
+++ client/src/components/Information/Information.jsx
@@ -32,7 +32,7 @@ function Information() {
                 <div className="description-www">
                     <div className="text-local-data font-styles">
                         <div className="calendar-svg" alt="ilustração de calendário roxo"></div>
-                        <p className="alinhar-data">{info[0]?.data.substring(0, 10)}</p>
+                        <p className="alinhar-data">{info[0]?.data}</p>
                     </div>
                     <div className="text-local-data adjust-this font-styles">
                         <div className="local-svg" alt="ilustração de calendário roxo"></div>
@@ -44,15 +44,18 @@ function Information() {
                     <h3>Premiações</h3>
                     <div className="each-prize">
                         <div className="second" alt="pódio segundo lugar">
-                            <h1 className="each-position podium-sec">2</h1>
+                            <h1 className="each-position podium-sec display-number">2</h1>
+                            <h1 className="mobile-position-number adjusting-display">1</h1>
                             <p>{info[0]?.premio2}</p>
                         </div>
                         <div className="first-prize" alt="pódio primeiro lugar">
-                            <h1 className="each-position podium-first">1</h1>
+                            <h1 className="each-position podium-first display-number">1</h1>
+                            <h1 className="mobile-position-number adjusting-display">2</h1>
                             <p>{info[0]?.premio1}</p>
                         </div>
                         <div className="third" alt="pódio terceiro lugar">
-                            <h1 className="each-position podium-third">3</h1>
+                            <h1 className="each-position podium-third display-number">3</h1>
+                            <h1 className="mobile-position-number adjusting-display">3</h1>
                             <p>{info[0]?.premio3}</p>
                         </div>
                     </div>
